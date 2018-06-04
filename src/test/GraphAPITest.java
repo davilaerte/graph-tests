853a74eaced814eaaf90b12586d45d05c4080d32
Davi Laerte <davi.nascimento@ccc.ufcg.edu.br>
2018-06-04 01:29:40 -0300

    New tests added

--- src/test/GraphAPITest.java
+++ src/test/GraphAPITest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 import implementation.Graph;
 import implementation.GraphAPIInterface;
 import implementation.GraphAPIInterfaceImpl;
+import implementation.GraphUtils;
 
 public class GraphAPITest {
 	
@@ -75,6 +76,60 @@ public class GraphAPITest {
 	}
 	
 	@Test
+	public void testGetEdgeNumber() {
+		Graph graphOne = mockGraph();
+		int expectedNumberEdges;
+		int numberAddedEdges;
+		
+		expectedNumberEdges = GraphUtils.getEdgeNumber(graphOne);
+		numberAddedEdges = 4;
+		
+		Assert.assertEquals(expectedNumberEdges, numberAddedEdges);
+	}
+	
+	@Test
+	public void testGetEdgeNumberTwo() {
+		Graph graphOne = mockGraphDisconnected();
+		int expectedNumberEdges;
+		int numberAddedEdges;
+		
+		expectedNumberEdges = GraphUtils.getEdgeNumber(graphOne);
+		numberAddedEdges = 2;
+		
+		Assert.assertEquals(expectedNumberEdges, numberAddedEdges);
+	}
+	
+	@Test
+	public void testGetMeanEdge() {
+		Graph graphOne = mockGraph();
+		float expectedMeanEdge;
+		float numberMeanEdge;
+		float delta = 0;
+		
+		/* An integer division is being executed inside the method, since there
+		 * is no conversion of one of the division values ​​to float or double,
+		 * so the mean degree will always be an integer value.
+		 */
+		expectedMeanEdge = GraphUtils.getMeanEdge(graphOne);
+		numberMeanEdge =  1.6f;
+		
+		Assert.assertEquals(expectedMeanEdge, numberMeanEdge, delta);
+	}
+	
+	@Test
+	public void testGetMeanEdgeTwo() {
+		Graph graphOne = mockGraphFigureOne();
+		float expectedMeanEdge;
+		float numberMeanEdge;
+		float delta = 0;
+		
+		expectedMeanEdge = GraphUtils.getMeanEdge(graphOne);
+		numberMeanEdge =  2.0f;
+		
+		Assert.assertEquals(expectedMeanEdge, numberMeanEdge, delta);
+	}
+	
+	@Test
 	public void testBFS() {
 		Graph graph = mockGraphFigureOne();
 		int firstVertex = 1;
@@ -97,6 +152,10 @@ public class GraphAPITest {
 		/*
 		 * There is a possible bug in method DFS, the initial vertex (passed as parameter)
 		 * is not parent of any other vertex.
+		 * 
+		 * The initial vertex index is not being accessed correctly because it is not
+		 * considering that the initial vertex value is equal to the index + 1, so
+		 * the parent is not being correctly stored for this reason.
 		 */
 		String realDFS = this.graphManipulator.DFS(graphOne, firstVertex);
 		String expectedDFS =  "1-0 -\n" +
@@ -121,9 +180,18 @@ public class GraphAPITest {
 	}
 	
 	@Test
-	public void testConnected() {
+	public void testDisconnectedGraph() {
 		Graph graph = mockGraphDisconnected();
 		boolean expected = false;
+		boolean real = this.graphManipulator.connected(graph);
+		
+		Assert.assertEquals(expected, real);
+	}
+	
+	@Test
+	public void testConnectedGraph() {
+		Graph graph = mockGraphFigureOne();
+		boolean expected = true;
 		boolean real = this.graphManipulator.connected(graph);
 		
 		Assert.assertEquals(expected, real);
@@ -206,6 +274,20 @@ public class GraphAPITest {
 			fail("Error in reading file");
 		}
 	}
+	
+	@Test
+	public void mst() {
+			Graph graph =  mockGraphWithWeightTwo();
+			
+			String answer = this.graphManipulator.mst(graph);
+			
+			String expected = "1 - - 0\n" + 
+					"2 - 1 1\n" + 
+					"3 - 4 4\n" + 
+					"4 - 5 3\n" + 
+					"5 - 2 2";
+			Assert.assertEquals(expected, answer);
+	}
 		
 	private Graph mockGraph() {
 		Graph graph = new Graph(5);
@@ -241,7 +323,25 @@ public class GraphAPITest {
 		return graph;
 	}
 	
-	
+	private Graph mockGraphWithWeightTwo() {
+		Graph graph = new Graph(5);
+		
+		graph.addEdge(1, 2, 0.1);
+		graph.addEdge(2, 1, 0.1);
+		graph.addEdge(2, 5, 0.2);
+		graph.addEdge(5, 2, 0.2);
+		graph.addEdge(5, 3, 5.0);
+		graph.addEdge(3, 5, 5.0);
+		graph.addEdge(3, 4, 1.0);
+		graph.addEdge(4, 3, 1.0);
+		graph.addEdge(4, 5, 2.3);
+		graph.addEdge(5, 4, 2.3);
+		graph.addEdge(1, 5, 1.0);
+		graph.addEdge(5, 1, 1.0);
+		
+		return graph;
+	}
+		
 	private Graph mockGraphFigureOne() {
 		Graph graph = new Graph(5);
 
